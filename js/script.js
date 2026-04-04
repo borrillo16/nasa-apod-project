0dad0ad45fa0101413dc86d8970503b35c465a06
Mario Borri <borrillo16@gmail.com>
2026-04-04 20:04:59 +0200

    Añadida función buscar + filtrado

--- js/script.js
+++ js/script.js
@@ -1,35 +1,55 @@
 const API_KEY = "0VeCpkDeaBHszSFV9Zfn10R9kUohNHWITF3RBPnJ";
 const API_URL = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&count=30`;
 
-
+let fotosOriginales = [];
 
 async function obtenerFotos() {
-    try{
+    try {
         const respuesta = await fetch(API_URL);
         const datos = await respuesta.json();
-        console.log ("Datos recibidos de la NASA:", datos);
-        pintarFotos(datos);
-    }catch(error){
+        fotosOriginales = datos;
+        console.log("Datos recibidos de la NASA:", datos);
+        pintarFotos(fotosOriginales);
+    } catch (error) {
         console.error("Error al conectar con la NASA:", error);
     }
-   
+
 }
 
 function pintarFotos(fotos) {
     const contenedor = document.getElementById("lista-fotos");
     contenedor.innerHTML = "";
-    fotos.forEach((item, index) => {
+
+      if (fotos.length === 0) {
+    contenedor.innerHTML = "<p class='error'>No se han encontrado resultados.</p>";
+  }
+
+    fotos.forEach((item) => {
         contenedor.innerHTML += `
             <article class="tarjeta">
-                <img src="${item.url}" alt="${item.title}" style="width: 100px;">
-                <h3>${item.title}</h3>
-                <p>Fecha: ${item.date}</p>
-                <p>Copyright: ${item.copyright || 'Público'}</p>
-                
-                <a href="detalle.html?date=${item.date}">Ver más detalles</a>
-            </article>
-            `
-    
+                <img src="${item.url}" alt="${item.title}">
+                <div class="info">
+                    <h3>${item.title}</h3>
+                    <ul>
+                        <li><strong>Fecha:</strong> ${item.date}</li>
+                        <li><strong>Autor:</strong> ${item.copyright || "NASA / Dominio público"}</li>
+                        <li><strong>Tipo:</strong> ${item.media_type.toUpperCase()}</li>
+                        <li><a href="${item.hdurl}" target="_blank">Descargar en HD"</a></li>
+                    </ul>
+                    <a class="boton-detalle" href="detalle.html?date=${item.date}">Ver explicación completa</a>
+                </div>
+
+            </article> `
+
     });
 }
-  obtenerFotos();
+
+buscador.addEventListener("input", (event) => {
+    const texto = event.target.value.toLowerCase().trim();
+    const filtrados = fotosOriginales.filter((fotos) => 
+        fotos.title.toLowerCase().includes(texto)
+    );
+    pintarFotos(filtrados);
+})
+
+obtenerFotos();
